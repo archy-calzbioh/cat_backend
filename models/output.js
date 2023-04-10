304d43e81c01243555ede18d0ad6e3b36f033740
king-archy <king.archy.13013@gmail.com>
2023-04-09 21:41:53 -0700

    updated schema

--- models/output.js
+++ models/output.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
 const outputSchema = new mongoose.Schema({
-    question: String,
-    answer: String
-})
+  question: String,
+  answer: String,
+  imageUrl: String, // Add a new field for the generated image URL
+});
+
 
 const OutputModel = mongoose.model('Output', outputSchema);
 
